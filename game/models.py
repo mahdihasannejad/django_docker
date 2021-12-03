3adc534a1bc4b8fbc621d0161e7826743074ee05
Amirreza <amirreza.abdi.b@gmail.com>
2021-12-03 08:59:25 -0500

    ...

--- game/models.py
+++ game/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Game_sale(models.Model):
-    name = models.CharField(max_length=200)
+class Games(models.Model):
+    Rank= models.SmallIntegerField(default=0)
+    Name = models.CharField(max_length=200)
     Platform = models.CharField(max_length=10)
-    year = models.SmallIntegerField(default= 0)
-    genre =  models.CharField(max_length=20)
+    Year = models.SmallIntegerField(default= 0)
+    Genre =  models.CharField(max_length=20)
     Publisher = models.CharField(max_length=20)
     NA_Sales = models.DecimalField(max_digits=6, decimal_places=3,default= 0.0)
     EU_Sales = models.DecimalField(max_digits=6, decimal_places=3,default= 0.0)
--- game/views.py
+++ game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from game.models import Game_sale
+from game.models import Games
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -22,37 +22,37 @@ def index(request):
             rows.append(row)
 
         for i in range(1,len(rows)):
-            b = Game_sale(name=rows[i][1],Platform = rows[i][2],year = float(rows[i][3]) ,genre =rows[i][4],
+            b = Games(Rank=float(rows[i][0]),Name=rows[i][1],Platform = rows[i][2],Year = float(rows[i][3]) ,Genre =rows[i][4],
                 Publisher=rows[i][5],NA_Sales=float(rows[i][6]),EU_Sales=float(rows[i][7]),JP_Sales=float(rows[i][8]),
                 Other_Sales = float(rows[i][9]),Global_Sales =float(rows[i][10]))
             b.save()
         return Response({"Post.": "succsus"})
-    Game_sale.objects.all().delete()
+    Games.objects.all().delete()
     return Response({"Hello, world. You're at the polls index."})
 
 @api_view(['GET', 'POST'])
 def topplatform(request):
     if request.method == 'POST':
         topN = int(request.data['N'])
-        platforms =Game_sale.objects.values_list('Platform')
+        platforms =Games.objects.values_list('Platform')
 
         set_platform = set(platform[0] for platform in platforms)
         a = {}
         for platform in set_platform:
-            a[platform] = (Game_sale.objects.filter(Platform=platform).order_by('-Global_Sales').values_list('name','Global_Sales')[0:topN])
+            a[platform] = (Games.objects.filter(Platform=platform).order_by('-Global_Sales').values_list('Name','Global_Sales')[0:topN])
         return Response(a)
     return Response({'error':"request not post"})
 
 @api_view(['GET', 'POST'])
 def ir_rank(request):
     if request.method == 'POST':
-        return Response({'rank': request.data['rank']})
+        return Response({'Rank': request.data['Rank']})
     return Response({'error':"request not post"})
 
 @api_view(['GET', 'POST'])
 def ir_name(request):
     if request.method == 'POST':
-        return Response({'rank': request.data['name']})
+        return Response({'Rank': request.data['Name']})
     return Response({'error':"request not post"})
 
 @api_view(['GET', 'POST'])
@@ -70,7 +70,7 @@ def topCategory(request):
 @api_view(['GET', 'POST'])
 def topYear_platform(request):
     if request.method == 'POST':
-        return Response({'year': request.data['year'],'platform':request.data['platform']})
+        return Response({'Year': request.data['Year'],'Platform':request.data['Platform']})
     return Response({'error':"request not post"})
 
 @api_view(['GET'])
